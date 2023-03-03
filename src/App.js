92e7bf14aee0145b90e5ef9062a6af4921388c0f
James Aules <james_aules@icloud.com>
2023-03-03 16:39:19 -0500

    test validations

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 
 function App() {
-  return (
+
+const [gitHubName, setGitHubName] = useState('')           
+const [githubUrl, setGitHubUrl] = useState('')          
+
+
+useEffect(() => {
+    fetch('https://api.github.com/users/learningToCode1234')
+    .then(res => res.json())
+    .then(data => {
+        setGitHubName(data.name)
+        setGitHubUrl(data.html_url)
+    })
+}, [])
+
+return (
     <div className="App">
-      <h1>Github Profile Info:</h1>
+        <h1>Github Profile Info:</h1>
+        <h2>{gitHubName}</h2>
+        <a href={githubUrl}>
+          <button  >Link to github </button>
+        </a>
     </div>
-  );
+);
 }
 
-export default App;
+export default App
+    
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,27 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import App from './App'
+
+beforeEach(() => {
+    // sets everything back to initial state before each test
+    fetch.resetMocks();
+  })
 
 
+describe('receives values from GitHub REST API using jest fetch mock', () => {
+    test("receives GitHub name", async () => {
+      fetch.mockResponseOnce(JSON.stringify({name: 'coder'}))
+      render(<App />)
+      const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+      expect(gitHubName).toHaveTextContent('coder')
+    })
+
+
+
+test('github url validation', async () => {
+    fetch.mockResponseOnce(JSON.stringify({ html_url : 'https://api.github.com/users/learningToCode1234'}))
+ render(<App/>)
+ const gitHubURL = await waitFor(() => screen.getByRole('link'))
+ expect(gitHubURL).toHaveAttribute('href', expect.stringContaining('github.com'))
+})
+})
 
